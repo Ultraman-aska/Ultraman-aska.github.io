c7b171edbdb48d2151d2a316794b029f10b4576e
Ultraman-aska <shachiko3145@gmail.com>
2025-04-15 16:23:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function sayHi() {
   const greetings = [
-    "はじめまして！Kaiです👋",
-    "コードで世界を変えよう！",
-    "今日も一歩前進！",
-    "Hello from Kai 🚀"
+    "☆大吉☆",
+    "☆中吉☆",
+    "☆小吉☆",
+    "☆末吉☆"
   ];
   const index = Math.floor(Math.random() * greetings.length);
   document.getElementById("message").textContent = greetings[index];
